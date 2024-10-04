0ee39828c6e4a4c152f3716606feb353478cd27b
vanshu778 <vthesiya778@gmail.com>
2024-10-04 13:35:15 +0530

    installing packages

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,5 +46,5 @@ const userSchema = new Schema({
     timestamps:true
 })
 
-
+ 
 export const User = mongoose.model("User",userSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 
 const videoSchema = new Schema({
     videoFile: {
@@ -37,5 +39,7 @@ const videoSchema = new Schema({
 {
     timestamps:true
 })
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema)
