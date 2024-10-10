272ddaaefd1a6203fe2c4b5416e29674305c9baa
vanshu778 <vthesiya778@gmail.com>
2024-10-10 19:19:02 +0530

    password encrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, {Schema} from "mongoose";
-
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+ 
 const userSchema = new Schema({
     username:{
         type:String,
@@ -46,5 +48,15 @@ const userSchema = new Schema({
     timestamps:true
 })
 
- 
+ userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next();
+     
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
 export const User = mongoose.model("User",userSchema)
