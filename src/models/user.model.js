89cea39e7be4e088391f21e411e90446cd279e34
vanshu778 <vthesiya778@gmail.com>
2024-10-10 20:02:46 +0530

    tokens generate

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new Schema({
         lowercase:true,
         trim:true,
     },
-    fullname:{
+    fullName:{
         type:String,
         required:true,
         trim:true,
@@ -57,6 +57,31 @@ const userSchema = new Schema({
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+)
+}
+
+userSchema.methods.generateRefreshToken = function(){return jwt.sign({
+    _id: this._id,
+},
+process.env.REFRESH_TOKEN_SECRET,
+{
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+}
+)
 }
 
 export const User = mongoose.model("User",userSchema)
