c3076e729e0b0ab17a2c71ba17eb18c26596df08
vanshu778 <vthesiya778@gmail.com>
2024-10-15 13:58:35 +0530

    complete file upload part

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,47 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret:process.env.CLOUDINARY_API_SECRET// Click 'View API Keys' above to copy your API secret
 });
+
+import { v2 as cloudinary } from 'cloudinary';
+
+( async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'backend-vanshu', 
+        api_key: '863283753172711', 
+        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+    }
+);
+    
+const uploadOnCloudinary = async (localFilePath) => {
+    try 
+    {
+        if (!localFilePath) return null
+          //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully
+        console.log("file is uploaded on cloudinary",response.url);
+        return response
+    }
+    catch (error){
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+// Upload an image
+const uploadResult = await cloudinary.uploader
+    .upload(
+        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+         public_id: 'shoes',
+        }
+    )
+    .catch((error) => {
+        console.log(error);
+    });
+    
+console.log(uploadResult);
+})();   
