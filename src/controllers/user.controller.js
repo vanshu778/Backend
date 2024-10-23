a4964171865ec7cf9a441ec5d5438988d6bb04ac
vanshu778 <vthesiya778@gmail.com>
2024-10-23 11:08:01 +0530

    user exist or not step

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler(async(req,res) => {
     // res.status(200).json({
@@ -27,6 +28,14 @@ const registerUser = asyncHandler(async(req,res) => {
         [fullName, email, username, password].some((field) => field?.trim() === "")
     ){
         throw new ApiError(400,"All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exists")
     }
 })
 
