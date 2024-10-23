208ec06bb227a9c9801977e9faf735d4a3b9d457
vanshu778 <vthesiya778@gmail.com>
2024-10-23 14:05:34 +0530

    not use await in existedUser so name not found

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,9 +32,10 @@ const registerUser = asyncHandler(async(req,res) => {
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username},{email}]
     })
+    
     if(existedUser){
         throw new ApiError(409,"User with email or username already exists")
     }
