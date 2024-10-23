fe8c112e2717cc91fd2bf4027a8ad12031fd18b5
vanshu778 <vthesiya778@gmail.com>
2024-10-23 10:41:10 +0530

    logic building steps for register user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,18 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async(req,res) => {
-    res.status(200).json({
-        message:"ok"
-    })
+    // res.status(200).json({
+    //     message:"ok"
+    // })
+    // step-1 get user detail from frontend
+    // step-2 validation - not empty 
+    // step-3 check if user already exists: username, email 
+    //step-4 check for images, check of avatar
+    //step-5 upload them to cloudinary , avatar
+    // step-6 create user object - create entry in db
+    // step-7 remove password and refresh token field from response
+    // step-8 check for user creattion 
+    // step-9 return res
 })
 
 export {registerUser}
