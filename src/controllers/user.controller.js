1d92e37287c6832a892e0b770e521ccc1efac231
vanshu778 <vthesiya778@gmail.com>
2024-10-23 12:19:25 +0530

    created user object

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -48,6 +48,21 @@ const registerUser = asyncHandler(async(req,res) => {
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar:avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+
 })
 
 export {registerUser}
