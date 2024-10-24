07b3b946ee7ba94e04a6c8f85f3ff6557d2804a7
vanshu778 <vthesiya778@gmail.com>
2024-10-24 08:01:27 +0530

    loggedIn user ready

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,8 +139,31 @@ const generateAccessAndRefreshTokens = async(userId) => {
 
     const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
 
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user:loggedInUser, accessToken,refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
   })
-export {registerUser}
+export {
+    registerUser,
+    loginUser
+}
 
 
 
