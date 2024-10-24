7cd4e15fb716380689a26be069c1f31bd0dc4d17
vanshu778 <vthesiya778@gmail.com>
2024-10-24 08:53:30 +0530

    Basic error solved

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
-import { loginUser,registerUser } from "../controllers/user.controller.js";
+import { loginUser,logoutUser,registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
-
+import { verifyJWT } from "./../middlewares/auth.middleware.js";
 const router = Router()
 router.route("/register").post(
     upload.fields([
