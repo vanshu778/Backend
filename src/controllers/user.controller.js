a6f12b00ee26183fd83fb06f6d840ca5d92e6b55
vanshu778 <vthesiya778@gmail.com>
2024-10-24 08:43:45 +0530

    create logOut user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -161,8 +161,35 @@ const generateAccessAndRefreshTokens = async(userId) => {
     )
   })
 
+  const logoutUser = asyncHandler(async(req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User logged out"))
+  })
+  
 export {
     registerUser,
+    loginUser,
+    logoutUser
 }
 
 
