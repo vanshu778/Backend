8c1439a89d7a61da4f8884588c86c4cbf77ab5bf
vanshu778 <vthesiya778@gmail.com>
2024-10-27 12:23:10 +0530

    getWatchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefreshTokens = async(userId) => {
@@ -345,7 +346,7 @@ const generateAccessAndRefreshTokens = async(userId) => {
     )
   })
 
-  const getUserChannelProfile = asyncHandler(async(req,res) => {
+  const getUserChannelProfile = asyncHandler(async(req,res) => { 
     const {username} = req.params
 
     if(!username?.trim()){
@@ -416,6 +417,58 @@ const generateAccessAndRefreshTokens = async(userId) => {
     )
   })
 
+  const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+      {
+        $match:{
+          _id:new mongoose.Types.ObjectId(req.user._id)
+        }
+      },{
+        $lookup:{
+          from:"videos",
+          localField:"watchHistory",
+          foreignField:"_id",
+          as:"watchHistory",
+          pipeline:[
+            {
+              $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:[
+                  {
+                    $project:{
+                      fullName:1,
+                      username:1,
+                      avatar:1
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              $addFields:{
+                owner:{
+                  $first:"$owner"
+                }
+              }
+            }
+          ]
+        }
+      }
+    ])
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch history fetched successfully"
+      )
+    )
+  })
+
   export {
     registerUser,
     loginUser,
@@ -426,5 +479,6 @@ const generateAccessAndRefreshTokens = async(userId) => {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
